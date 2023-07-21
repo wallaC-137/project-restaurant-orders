be6fab43ecc8a782ec5110531f1ee2a26b02519c
wallaC-137 <wallace_031@hotmail.com>
2023-07-21 19:20:32 -0300

    req01: ok - cria testes para o modulo ingredient.py

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,37 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("farinha")
+    ingredient2 = Ingredient("farinha")
+    assert hash(ingredient1) == hash(ingredient2)
+
+    ingredient3 = Ingredient("farinha")
+    ingredient4 = Ingredient("bacon")
+    assert hash(ingredient3) != hash(ingredient4)
+
+    ingredient5 = Ingredient("farinha")
+    ingredient6 = Ingredient("farinha")
+    assert ingredient5 == ingredient6
+
+    ingredient7 = Ingredient("farinha")
+    ingredient8 = Ingredient("bacon")
+    assert ingredient7 != ingredient8
+
+    ingredient9 = Ingredient("farinha")
+    assert repr(ingredient9) == "Ingredient('farinha')"
+
+    ingredient10 = Ingredient("farinha")
+    assert ingredient10.name == "farinha"
+
+    ingredient11 = Ingredient("queijo mussarela")
+    assert ingredient11.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    ingredient12 = Ingredient("farinha")
+    assert isinstance(ingredient12, Ingredient)
